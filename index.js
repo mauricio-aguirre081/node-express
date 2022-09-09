77c06a556ad43df48013860c30082dd85dedeecb
Mauricio Aguirre <mauricioaguirrert@yahoo.com>
2022-09-09 18:12:23 -0300

    Peticiones http

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-// SERVIDOR SIN USAR EXPRESS
-/* const http = require ('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const reading = fs.createReadStream('./static/index.html')
-    reading.pipe(res)
-});
-
-server.listen(3000)
-console.log(`server on port ${3000}`) */
 
 const express = require ('express')
 const app = express()
+
+app.get('/products', (req, res) => {
+    res.send('lista de productos')
+})
+
+app.post('/products', (req, res) => {
+    res.send('creando productos')
+})
+
+app.put('/products', (req, res) => {
+    res.send('actualizando un producto')
+})
+
+app.delete('/products', (req, res) => {
+    res.send('eliminando un producto')
+})
+
+app.patch('/products', (req, res) => {
+    res.send('actualizando una parte del producto')
+})
+
+
+
 app.listen(3000)
-
-app.get('/', (req, res) => {
-    res.send('hola tarolas')
-})
-
-app.get('/about', (req, res) => {
-    res.send('ABOUT')
-})
-
-app.get('/weather', (req, res) => {
-    res.send('the current weather is nice')
-})
-
-app.use((req, res) => {
-    res.status(404).send('No se encontró tu página')
-})
-
-console.log(`server on por ${3000}`)
+console.log(`server on port ${3000}`)
