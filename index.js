fb49a44d514b3557eeaaf33eee81ad980482c8c4
Mauricio Aguirre <mauricioaguirrert@yahoo.com>
2022-09-09 21:55:58 -0300

    Request Params

--- index.js
+++ index.js
@@ -3,14 +3,29 @@
 const express = require ('express')
 const app = express()
 
-app.use(express.text())
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
-app.post('/usernuevo', (req, res) =>{
-    console.log(req.body)
-    res.send('nuevo usuario creado')
+app.get('/hello/:username', (req, res) => {
+    res.send(`Hello ${req.params.username.toUpperCase()}`)
 })
+
+app.get('/add/:a/:b', (req, res) => {
+    const total = parseInt(req.params.a) + parseInt(req.params.b);
+    res.send(`The Result is: ${total}`)
+})
+
+app.get('/users/:username/photo', (req, res) => {
+    if(req.params.username === "Julio") {
+        return res.sendFile('./heart.png', {
+            root: __dirname
+        })
+    }
+    res.send('El usuario no tiene acceso')
+})
+
+app.get('/name/:nombre/age/:edad', (req, res) => {
+    res.send(`El usuario ${req.params.nombre} tiene ${req.params.edad}`)
+})
+
+
 
 
 app.listen(3000)
